5d0f5f6cfe129aaf08f80ac90639e64f8013a79c
ali <eng.aliali2011@gmail.com>
2023-01-29 00:03:01 +0200

    save new student

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import { useMutation, useQuery } from '@apollo/client'
 
-import { CreateNewStudentDocument, NewOrganizationDocument, StudentsDocument, StudentsQuery, StudentsQueryVariables } from '@/gql/graphql'
+import { CreateNewStudentDocument, NewOrganizationDocument, StudentInputInput, StudentsDocument, StudentsQuery, StudentsQueryVariables } from '@/gql/graphql'
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import { Box, Button, Divider, Input, Modal, Stack, TextField, Typography } from '@mui/material'
 import { useState } from 'react'
 import { Field, FieldArray, Form, FormLoader, getFieldProps, Reset, ResponseMessage, Submit } from 'apollo-form'
 import * as Yup from 'yup';
+import { redirect } from 'next/navigation';
+import { useRouter } from 'next/router'
 
 export default function Home() {
 
@@ -25,10 +27,11 @@ export default function Home() {
     p: 4,
   };
   
+  const router = useRouter()
+
   const [open, setOpen] = useState(false);
 
   const { data, error, loading } = useQuery(StudentsDocument);
-
 
   if (error) {
     console.log('Error')
@@ -38,48 +41,25 @@ export default function Home() {
     console.log('Loading')
   }
 
-
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-
-  const rows = [
-    { id: 1, lastNassme: 'Snow', firstNssasme: 'Jon' },
-    { id: 2, lastNasme: 'Lannister', firsstsName: 'Cersei' },
- 
-  ];
-  
-  const handleOpen =  () => { 
-     setOpen(true);
-  };
-
-  const handleClose =  () => { 
-    setOpen(false);
- };
-
  const [addTodo] = useMutation(CreateNewStudentDocument);
+ 
+ const createStudentMutation =  (values: any) => {
+  console.log(values)
+  const studentJson={
+    "studentName": values.values.studentName,
+    "testscores": [
+    ]
+   }
 
- const createStudentMutation =  (data: any) => { 
-
-  console.log(data.values);
-   addTodo(data.values);  
+  const student: any={
+    "values": {
+      "studentIdentifier": "5555555",
+      "studentJson": studentJson
+    }
+  }
+  
+   console.log(values)
+   addTodo({ variables: student });  
    setOpen(false);
  };
 
@@ -95,6 +75,15 @@ const initialState = {
   studentName: ''
 };
 
+const newStudentPage =  () => {
+  router.push('newStudent');
+};
+
+const editStudentPage =  (id: number) => {
+  console.log(id)
+  router.push({pathname: 'editStudent/[id]' , query: { id: id }});
+};
+
 
   return (
     <>
@@ -104,80 +93,29 @@ const initialState = {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Button 
+          variant="contained"
+          onClick={newStudentPage}>
+            New Student
+      </Button>
+
           { data?.students?.map(item=> { 
             return (
               <> 
                 <Stack direction='row'>
                   <Typography> { item?.id} </Typography>
-                  <Typography> { item?.studentJson} </Typography>
                   <Typography> { item?.studentIdentifier} </Typography>
+                  <Typography> { item?.studentJson} </Typography>
                   <Button 
                     variant="contained"
-                    onClick={handleOpen}>
-                      New Student
-                  </Button>  
+                    onClick={(e)=> editStudentPage(item?.id)}>
+                      Edit
+                  </Button>
                 </Stack>  
-               
-
               </>
             );
            })} 
-          
-          <Modal
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-          >
-            <Box sx={styled}>
-              <Box>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Create new Student
-                </Typography>
-              </Box> 
-              <Divider/>
-                <Form<CreatePlanFormValues>
-                  name='CreatePlanForm'
-                  enableReinitialize
-                  initialState={initialState}
-                  validationSchema={validationSchema}
-                  onSubmit={async ({ values }, form) => {
-                      try {
-                        await createStudentMutation({ values  });
-                        form.reset();
-                      } catch (e) {
-                        console.log('error');
-                      }
-                  }}
-                >
-                  <Box sx={{ pt : 3}}>
-                    <Field name='studentName'>{({ field }) => <Input {...getFieldProps(field)} placeholder='Student Name' />}</Field>
-                    <ResponseMessage>{({ error }) => <span>{error}</span>}</ResponseMessage>
-                  </Box>  
-                  <FormLoader>
-                      {({ loading }) => (
-                        <span style={{ display: loading ? 'block' : 'none' }}>Loading...</span>
-                      )}
-                  </FormLoader>
-                  <Box sx={{ pt : 3}}>
-                    <Submit>
-                        {({ disabled }) => (
-                          <Button type='submit' disabled={disabled} >
-                              Create Student
-                          </Button>
-                        )}
-                    </Submit>
-                    <Reset>
-                        {({ disabled }) => (
-                          <Button type='reset' disabled={disabled}  color="error" >
-                              Reset
-                          </Button>
-                        )}
-                    </Reset>
-                  </Box>  
-                </Form>             
-              </Box>
-          </Modal>
     </>
   )
 }
