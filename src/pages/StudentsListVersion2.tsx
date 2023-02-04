294a38651c2ceb8308a0bcaea0e5003f122980ca
echasin <echasin@innvosolutions.com>
2023-02-04 11:01:46 -0800

    Added - Data Grid and Direct GQL - Datagrid displays Students data - Removed Generated GraphQL code

--- src/pages/StudentsListVersion2.tsx
+++ src/pages/StudentsListVersion2.tsx
@@ -2,13 +2,20 @@ import * as React from 'react';
 
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs'
+import Card from '@mui/material/Card';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import Grid from '@mui/material/Grid';
+import Layout from 'components/layouts/layout'
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+
+import { useMutation, useQuery, gql } from '@apollo/client'
+import { StudentsDocument } from '@/gql/graphql'
+
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,23 +25,80 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
-export default function StudentList() {
+
+//RETREIVE DATA USING GRAPHQL DEFINED QUERY
+const QUERY_GETSTUDENTS = gql`
+ query students {
+     students {
+       id
+       studentIdentifier
+       studentJson
+     }
+   }
+`;
+
+//CONFIG DATA GRID
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+        field: 'studentIdentifier',
+        headerName: 'Student Identifier',
+        width: 150,
+        editable: true,
+    },
+    {
+        field: 'studentJson',
+        headerName: 'Student JSON',
+        width:  750,
+        editable: true,
+    }
+];
+
+export default function StudentListVersion2() {
+
+    //RETREIVE DATA USING GRAPHQL FROM GENERATED CODE
+    // const { data, error, loading } = useQuery(StudentsDocument);
+
+    //RETREIVE DATA USING GRAPHQL DEFINED QUERY
+    const { data, error, loading } = useQuery(QUERY_GETSTUDENTS);
+    console.log("data:", data);
+
+    //MESSAGE FOR QUERY LOADING
+    if (loading) {
+        return <h2>Loading...</h2>;
+    }
+
+    //ERROR HANDLING
+    if (error) {
+        console.error("ERROR QUERY_GETSTUDENTS", error);
+        return <h2>ERROR...</h2>;
+    }
+
+    //SET student DATA OBJECT
+    const students = data.students;
+    console.log("students:", students);
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={8}>
-                    <Item>xs=8</Item>
-                </Grid>
-                <Grid item xs={4}>
-                    <Item>xs=4</Item>
-                </Grid>
-                <Grid item xs={4}>
-                    <Item>xs=4</Item>
-                </Grid>
-                <Grid item xs={8}>
-                    <Item>xs=8</Item>
-                </Grid>
-            </Grid>
-        </Box>
+        <div className='main-content'>
+            <Container>
+                Hello World
+                {/* <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} /> */}
+                <Box sx={{ height: 400, width: '100%' }}>
+                    <DataGrid
+                        rows={students}
+                        columns={columns}
+                        pageSize={5}
+                        rowsPerPageOptions={[5]}
+                        checkboxSelection
+                        disableSelectionOnClick
+                        experimentalFeatures={{ newEditingApi: true }}
+                    />
+                </Box>
+
+            </Container>
+        </div>
+
+
+
     );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 //import '@/styles/globals.css'
+
 import InnvoApolloProvider from 'graphql/apollo'
 import type { AppProps } from 'next/app'
 
